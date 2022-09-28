d773dfcb3fc7c4942870d246b517a5b603fadc60
jekiekolya <108493349+jekiekolya@users.noreply.github.com>
2022-09-28 20:36:03 +0000

    Deploying to gh-pages from @ jekiekolya/goit-js-hw-11@b6ac000a8f59b636cc485e800d421f93273b958a 🚀

--- index.d40f6630.js
+++
@@ -1,2 +0,0 @@
-const e={form:document.querySelector(".search-form")};e.form.addEventListener("submit",(function(e){e.preventDefault()})),console.log(e.form);
-//# sourceMappingURL=index.d40f6630.js.map
--- index.f86ffed6.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={form:document.querySelector(".search-form")};e.form.addEventListener("submit",(function(e){e.preventDefault()})),console.log(e.form)}();
-//# sourceMappingURL=index.f86ffed6.js.map
