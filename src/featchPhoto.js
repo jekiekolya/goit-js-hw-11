631f2f3e821e3eb3d964dcc547ef9992e5fd25d5
Mykola Berezhniuk <bermukola@gmail.com>
2022-10-03 21:39:57 +0300

    Update featchPhoto.js

--- src/featchPhoto.js
+++ src/featchPhoto.js
@@ -2,11 +2,37 @@ const axios = require('axios');
 const BASE_URL = 'https://pixabay.com/api/';
 const KEY = '30238693-0e670d13d8b6be1c19eb21932';
 
+// export default async function fetchPhoto(searchValue, numberPage) {
+//   try {
+//     const response = await axios.get(
+//       `${BASE_URL}?key=${KEY}&q=${searchValue}&image_type=photo&orientation=horizontal&safesearch=true&page=${numberPage}&per_page=40`
+//     );
+//     return response;
+//   } catch (error) {
+//     return error;
+//   }
+// }
+
+// Second logic with object config
 export default async function fetchPhoto(searchValue, numberPage) {
+  const config = {
+    // headers: {
+    //   '': '',
+    // },
+
+    params: {
+      key: KEY,
+      q: searchValue,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: numberPage,
+      per_page: 40,
+    },
+  };
+
   try {
-    const response = await axios.get(
-      `${BASE_URL}?key=${KEY}&q=${searchValue}&image_type=photo&orientation=horizontal&safesearch=true&page=${numberPage}&per_page=40`
-    );
+    const response = await axios.get(`${BASE_URL}`, config);
     return response;
   } catch (error) {
     return error;
